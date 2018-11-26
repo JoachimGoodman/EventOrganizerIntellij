d419e700ea8abb5d528037a1831b4e761a5cfedb
MarkusDrejer <mabd412b@stud.kea.dk>
2018-11-26 11:03:22 +0100

    Comments

--- src/Menu.java
+++ src/Menu.java
@@ -1,38 +1,38 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class Menu {
+public class Menu { // har programmets struktur
 
-    private JFrame frame;
-    private Container contentPane;
+    private JFrame frame; // primære window
+    private Container contentPane; // det som er inden for vinduet
 
     public Menu(){
-        frame = new JFrame("Event Organizer");
+        frame = new JFrame("Event Organizer"); // new object af vinduet, og giver det title
 
-        contentPane = frame.getContentPane();
+        contentPane = frame.getContentPane(); // indsætter en container ind i container
 
-        frame.setSize(800,600);
+        frame.setSize(800,600); // giver vinduet et størrelse
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // giver JFrame default settings. (tryk på kryds for at lukke programmet)
 
-        contentPane.add(login());
+        contentPane.add(login()); // tilføjer vores method som en panel ind i vores container
 
-        frame.setVisible(true);
+        frame.setVisible(true); // false som default. derfor gør vi det visible
     }
 
-    private JPanel login(){
-        JPanel setupPanel = new JPanel();
-        setupPanel.setLayout(null);
+    private JPanel login(){ // method som retunere et panel
+        JPanel setupPanel = new JPanel(); // nyt objekt
+        setupPanel.setLayout(null); // sætter layout til null for at kunne sætte tingene frit.
 
-        JTextField username = new JTextField();
+        JTextField username = new JTextField(); // newer et felt vi kan skrive i
         JTextField password = new JTextField();
-        JLabel label = new JLabel();
-        JButton confirm = new JButton("Confirm");
-        confirm.addActionListener(e -> {
-            String check = username.getText();
+        JLabel label = new JLabel(); // output text felt.
+        JButton confirm = new JButton("Confirm"); // laver knap med title
+        confirm.addActionListener(e -> { // bruger lambda udtryk for at simplificere actionlistener method
+            String check = username.getText(); // tager det tekst der står i textfeltet
             String check2 = password.getText();
-            if(!check.equals(DBLogin.checkUsername(check))){
-                label.setText("No such Username");
+            if(!check.equals(DBLogin.checkUsername(check))){  // bruger checkUsername methode for at checke om username stemmer over ens med det som er skrevet i tekstfeltet
+                label.setText("No such Username"); // opdaterer label methode
             }
             else{
                 if(check2.equals(DBLogin.checkPassword(check))){
@@ -44,11 +44,13 @@ public class Menu {
             }
         });
 
+        // koordinater og højde/bredde til tekst og knapper
         username.setBounds(300,200, 200, 20);
         password.setBounds(300,300,200,20);
         confirm.setBounds(300, 400, 200, 50);
         label.setBounds(400,100, 200, 20);
 
+        // tilføjer alt til panelet
         setupPanel.add(username);
         setupPanel.add(password);
         setupPanel.add(confirm);
