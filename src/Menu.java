dba6771fd979f0610a59f275ee57517948155d2a
MarkusDrejer <mabd412b@stud.kea.dk>
2018-11-27 11:36:05 +0100

    Refactoring of Menu and DBlogin classes

--- src/Menu.java
+++ src/Menu.java
@@ -6,6 +6,7 @@ public class Menu { // har programmets struktur
 
     private JFrame frame; // primære window
     private Container contentPane; // det som er inden for vinduet
+    private DBLogin loginAccess = new DBLogin();
 
     public Menu() {
         frame = new JFrame("Event Organizer"); // new object af vinduet, og giver det title
@@ -16,8 +17,10 @@ public class Menu { // har programmets struktur
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // giver JFrame default settings. (tryk på kryds for at lukke programmet)
 
-        //contentPane.add(login()); // tilføjer vores method som en panel ind i vores container
-        contentPane.add(SecreteryPanel());
+        contentPane.add(login()); // tilføjer vores method som en panel ind i vores container
+
+        /////////For Testing//////////////
+        //contentPane.add(secreteryPanel());
 
         frame.setVisible(true); // false som default. derfor gør vi det visible
     }
@@ -27,40 +30,15 @@ public class Menu { // har programmets struktur
         setupPanel.setLayout(null); // sætter layout til null for ikke at være låst af Javas standard layouts og kunne rykke ting frit rundt
 
         //Newer 2 textfields til username og password, med koordinatsæt og størrelse
-        JTextField username = new JTextField();
-        username.setBounds((frame.getWidth()/2)-(200/2), 225, 200, 20);
-        JTextField password = new JTextField();
-        password.setBounds((frame.getWidth()/2)-(200/2), 275, 200, 20);
+        JTextField usernameText = new JTextField();
+        usernameText.setBounds((frame.getWidth()/2)-(200/2), 225, 200, 20);
+        JTextField passwordText = new JTextField();
+        passwordText.setBounds((frame.getWidth()/2)-(200/2), 275, 200, 20);
 
         //Laver en Error label uden tekst da denne skal skiftes alt efter hvilken fejl der forekommer senere
+        //Opretter og tilføjer også yderligere labels til panelet
         JLabel LError = makeLabel("", -1, 310, 200, 20, 18); // Error label
         LError.setForeground(Color.RED);
-
-        //Laver en knap med koordinatsæt og størrelse
-        JButton confirm = makeButton("Confirm", (frame.getWidth()/2)-(200/2), 350, 200, 50, 20);
-        confirm.addActionListener(e -> { // bruger lambda udtryk for at simplificere actionlistener method
-            String check = username.getText(); // tager det tekst der står i textfeltet
-            String check2 = password.getText();
-            if (!check.equals(DBLogin.checkUsername(check))) {  // bruger checkUsername methode for at checke om username stemmer over ens med det som er skrevet i tekstfeltet
-                LError.setText("No such Username"); // opdaterer label methode
-            } else {
-                if (check2.equals(DBLogin.checkPassword(check))) {
-                    contentPane.remove(setupPanel); //fjerner loginsiden.
-                    contentPane.add(SecreteryPanel()); //ligger Arrangement op.
-                    frame.revalidate(); //det her skal man gøre. Den revaliderer.
-                    frame.repaint(); // det her skal man gøre. Den repainter.
-                } else {
-                    LError.setText("Wrong Password");
-                }
-            }
-        });
-
-        // tilføjer vores textfelter, knap og yderligere labels
-        setupPanel.add(username);
-        setupPanel.add(password);
-        setupPanel.add(confirm);
-        setupPanel.add(LError);
-
         //Title label
         setupPanel.add(makeLabel("PlanOrgan", -1, 100, 250, 100, 50));
         //Username label
@@ -68,10 +46,35 @@ public class Menu { // har programmets struktur
         //Password label
         setupPanel.add(makeLabel("Password", -1, 250, 200, 20, 18));
 
+
+        //Laver en knap med koordinatsæt og størrelse
+        JButton confirm = makeButton("Confirm", (frame.getWidth()/2)-(200/2), 350, 200, 50, 20);
+        confirm.addActionListener(e -> { // bruger lambda udtryk for at simplificere actionlistener method
+            String usernameInput = usernameText.getText(); // tager det tekst der står i textfeltet
+            String passwordInput = passwordText.getText();
+
+            if (!loginAccess.checkUsername(usernameInput)) {  // bruger checkUsername methode for at checke om username stemmer over ens med det som er skrevet i tekstfeltet
+                LError.setText("No such Username"); // opdaterer label methode
+            } else {
+                if (loginAccess.checkPassword(usernameInput, passwordInput)) {
+                    changePanel(setupPanel, secreteryPanel());
+                } else {
+                    LError.setText("Wrong Password");
+                }
+            }
+        });
+
+        // tilføjer vores textfelter og knap til panelet
+        setupPanel.add(usernameText);
+        setupPanel.add(passwordText);
+        setupPanel.add(confirm);
+        setupPanel.add(LError);
+
+        //Returnerer vores panel så det kan indsættes i vinduets container
         return setupPanel;
     }
 
-    private JPanel SecreteryPanel(){
+    private JPanel secreteryPanel(){
         JPanel setupPanel = new JPanel();
         setupPanel.setLayout(null);
 
@@ -79,8 +82,8 @@ public class Menu { // har programmets struktur
         JButton importButton = makeButton("Importer", 125, 100, 100, 25, 14);
         JButton exportButton = makeButton("Eksporter", 200, 100, 100, 25, 14);
 
-        for(int i = 0; i < DBLogin.getTitle().size(); i++){
-            setupPanel.add(makeLabel(DBLogin.getTitle().get(i),50, 200+i, 200, 20, 18));
+        for(int i = 0; i < loginAccess.getTitle().size(); i++){
+            setupPanel.add(makeLabel(loginAccess.getTitle().get(i),50, 200+i, 200, 20, 18));
         }
 
         setupPanel.add(makeLabel("Arrangement", 50, 50, 200, 20, 18));
@@ -115,7 +118,6 @@ public class Menu { // har programmets struktur
 
         return label;
     }
-
     //Metode til nemt at lave knapper
     private JButton makeButton(String title, int x, int y, int width, int height, int size) {
         JButton button = new JButton(title);
@@ -125,5 +127,12 @@ public class Menu { // har programmets struktur
         button.setForeground(Color.WHITE);
 
         return button;
+    }
+    //Metode til at skifte panel og opdatere vores vindue
+    private void changePanel(JPanel current, JPanel next){
+        contentPane.remove(current);
+        contentPane.add(next);
+        frame.revalidate();
+        frame.repaint();
     }
 }
