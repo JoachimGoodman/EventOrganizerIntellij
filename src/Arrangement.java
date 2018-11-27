fa6c364c3cd4fa5dd1db69d2f6d65f1688ff2d63
JoachimGoodman <joac1087@stud.kea.dk>
2018-11-27 11:35:14 +0100

    ændret

---
+++ src/Arrangement.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class Arrangement {
+
+    String name;
+    double totalPrice;
+    int duration;
+    int numberOfEvents;
+    int participations;
+
+    public Arrangement(String name, double totalPrice, int duration, int numberOfEvents, int participations){
+        this.name = name;
+        this.totalPrice = totalPrice;
+        this.duration = duration;
+        this.numberOfEvents = numberOfEvents;
+        this.participations = participations;
+    }
+
+
+}
