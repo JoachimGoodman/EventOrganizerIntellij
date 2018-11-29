98f708d53ca8696ded397731c2af3919d48cd115
MarkusDrejer <mabd412b@stud.kea.dk>
2018-11-30 00:56:18 +0100

    Reworked the database connection Made DBController class to making use of inheritance to easily open and close connections

--- src/DBLogin.java
+++ src/DBLogin.java
@@ -1,72 +1,58 @@
 import java.sql.*;
 
-public class DBLogin {
-
-    private static String url = "jdbc:sqlite:resources/EODatabase.db";
+public class DBLogin extends DBController {
 
     public boolean checkUsername(String username){
 
         boolean correctUsername = false;
+        ResultSet rs = super.resultsetQuery("SELECT USERNAME FROM LogIn WHERE USERNAME = '" + username + "'");
 
         try {
-            Connection conn = DriverManager.getConnection(url);
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT USERNAME FROM LogIn WHERE USERNAME = '" + username + "'");
+            if(username.equals(rs.getString("USERNAME"))) {
+                correctUsername = true;
+            }
 
-                if(username.equals(rs.getString("USERNAME"))) {
-                    correctUsername = true;
-                }
-
-            rs.close();
-            stmt.close();
-            conn.close();
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+        super.closeConnection(-1);
+
         return correctUsername;
     }
 
     public boolean checkPassword(String username, String password){
 
         boolean correctPassword = false;
+        ResultSet rs = super.resultsetQuery("SELECT PASSWORD FROM LogIn WHERE USERNAME = '" + username + "'");
 
         try {
-            Connection conn = DriverManager.getConnection(url);
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT PASSWORD FROM LogIn WHERE USERNAME = '" + username + "'");
-
             if(password.equals(rs.getString("PASSWORD"))) {
                 correctPassword = true;
             }
 
-            rs.close();
-            stmt.close();
-            conn.close();
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+        super.closeConnection(-1);
+
         return correctPassword;
     }
 
     public boolean isPowerUser(String username){
 
         boolean powerUser = false;
+        ResultSet rs = super.resultsetQuery("SELECT POWERUSER FROM LogIn WHERE USERNAME = '" + username + "'");
 
         try {
-            Connection conn = DriverManager.getConnection(url);
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT POWERUSER FROM LogIn WHERE USERNAME = '" + username + "'");
-
             if(rs.getInt("POWERUSER") == 1) {
                 powerUser = true;
             }
 
-            rs.close();
-            stmt.close();
-            conn.close();
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+        super.closeConnection(-1);
+
         return powerUser;
     }
 }
