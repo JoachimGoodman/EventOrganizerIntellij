dccf0c6aa888c2783cba06416447cd3e88b032dc
Isaksj <44366691+Isaksj@users.noreply.github.com>
2018-11-29 12:57:15 +0100

    Merge branch 'master' of C:\Users\Isak\Documents\GitHub\EventOrganizerIntellij with conflicts.

--- src/Event.java
+++ src/Event.java
@@ -1,14 +1,15 @@
 public class Event {
-    String name;
-    float totalPrice;
-    String description;
-    String eventtype;
-    String dateStart;
-    String dateEnd;
-    String facilitator;
-    String xDescription;
+    private String name;
+    private float totalPrice;
+    private String description;
+    private String eventtype;
+    private String dateStart;
+    private String dateEnd;
+    private String facilitator;
+    private String xDescription;
 
-    public Event(String name, float totalPrice, String description, String eventtype, String dateStart, String dateEnd, String facilitator, String xDescription) {
+    public Event(String name, float totalPrice, String description, String eventtype, String dateStart, String dateEnd, String facilitator, String xDescription)
+    {
         this.name = name;
         this.totalPrice = totalPrice;
         this.description = description;
@@ -18,4 +19,20 @@ public class Event {
         this.facilitator = facilitator;
         this.xDescription = xDescription;
     }
+
+        public String getEventName()
+        {
+            return name;
+        }
+        public String showEvent()
+        {
+            return name +
+                    totalPrice +
+                    description +
+                    eventtype +
+                    dateStart +
+                    dateEnd +
+                    facilitator +
+                    xDescription;
+        }
 }
